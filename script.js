d3b8c9003acad533b1e68f6808ce838ba046b8ca
valentinaskrobacki <valentinaskrobacki@gmail.com>
2024-10-14 16:24:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Menú
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const mainNav = document.getElementById('main-nav');
+
+    menuToggle.addEventListener('click', function() {
+        menuToggle.classList.toggle('active');
+        mainNav.querySelector('ul').classList.toggle('active');
+    });
+
+     // Cerrar el menú cuando se hace clic en una opción
+     const navLinks = document.querySelectorAll('.main-nav ul li a');
+     navLinks.forEach(link => {
+         link.addEventListener('click', function() {
+             // Ocultar el menú
+             menuToggle.classList.remove('active'); // Esto cerrará el menú
+             mainNav.querySelector('ul').classList.remove('active'); // Esto también cerrará el menú
+         });
+     });
+});
+
